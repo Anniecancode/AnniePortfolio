8f8f83ae9e6367f58fc43c3bb6b0cc44810f3c00
Anniecancode <99180884+Anniecancode@users.noreply.github.com>
2022-10-23 16:44:20 +1030

    added contents for home and contact page, also helper for checking valid email

---
+++ portfolio-app/src/components/pages/Home.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+export default function Home() {
+    return (
+        <div>
+
+            <h1>Annie Zhang</h1>
+
+            <h2 id="Who is Annie">Who is Annie</h2>
+
+            <img src="images/IMG_1.jpg" alt="Annie's selfie" />
+
+            <p>Annie is an accountant who is interested in coding!</p>
+            <p>I participated in a bootcamp organised by The University of Adelaide,  where I learnt HTML, CSS and Javascript from scratch.
+               In the bootcamp, I completed a number of mini projects, where showcases my skillset, and it also gives me the confidence to feel ready for any front-end rolls.
+               My mini projects can be found in "Annie's Works" section below,  please feel free to check on them.
+            </p>
+            <p>You like them? Then go to "Contact Annie" section, that's where you can find me!</p>
+
+            <ul>
+                <li>0400 000 000</li>
+                <li><a href="#contact"> anniez.wow@gmail.com</a></li>
+                <li><a href="https://github.com/Anniecancode">Github</a></li>
+                </ul>
+
+        </div>                           
+    )
+}
